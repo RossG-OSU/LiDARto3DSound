795704616d4e7f3ad2ba5f0a636a814ee187c171
RossG-OSU <122312916+RossG-OSU@users.noreply.github.com>
2024-10-20 12:19:23 -0700

    Fixed file upload from original repo I believe

---
+++ LiDARto3DSound/LiDARto3DSoundApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiDARto3DSoundApp.swift
+//  LiDARto3DSound
+//
+//  Created by Andrew on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct LiDARto3DSoundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
